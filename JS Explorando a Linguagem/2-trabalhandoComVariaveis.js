66fc709568f9c3f162f38785cea44e74f6357143
Allan08 <allan.felizzeti@ksys.com.br>
2022-09-08 09:17:24 -0300

    Tipos de Variáveis

---
+++ JS Explorando a Linguagem/2-trabalhandoComVariaveis.js
@@ -0,0 +1,10 @@
+ //Console.log("Trabalhando com Variáveis")
+ console.log("Trabalhando com Variáveis")
+ //JS é case sensitive
+
+ const idade = 29 
+ //cosnt Idade = 30
+const nome = "Ricardo"
+
+//não fazer
+// mas = "Janeiro"
---
+++ JS Explorando a Linguagem/atribuicaoVariaveis.js
@@ -0,0 +1,14 @@
+console.log("Trabalhando com atribuição de Variáveis")
+
+const idade = 29
+//let nome = "Ricardo" usando o let vc consegue passar outros valores para variavel 
+const primeiroNome = "Ricardo"
+const sobrenome = "Bugan"
+
+//console.log(nome + " " + sobrenome)
+console.log(primeiroNome, sobrenome)
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`)
+
+const nomeCompleto = primeiroNome + sobrenome
+
+console.log(nomeCompleto)
